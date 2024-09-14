0bb32a5646d1bb7016bb0e44c7f423febcf89820
tilthyx <tilthyx@gmail.com>
2024-09-13 23:47:00 -0300

    Feature Enemy - Make the enemy spawner random in the maps

---
+++ scenes/manager/enemy_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+const SPAWN_RADIUS = 375
+
+@export var basic_enemy_scene : PackedScene
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+		
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+	
+	var enemy = basic_enemy_scene.instantiate() as Node2D
+	get_parent().add_child(enemy)
+	enemy.global_position = spawn_position
+	
