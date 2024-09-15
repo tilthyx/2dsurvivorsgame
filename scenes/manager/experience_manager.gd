161d3b9332f1ba0152013196bab66dcceb2e591e
tilthyx <tilthyx@gmail.com>
2024-09-14 21:09:28 -0300

    Feature Experience - Making the progress bar for experience

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,14 +1,28 @@
 extends Node
 
+signal experience_updated(current_experience: float, target_experience: float, current_level: int)
+signal play_experience_bar_animation()
+
+const TARGET_EXPERIENCE_GROWTH = 5
+
 var current_experience = 0
+var current_level = 1
+var target_experience = 5
+
 
 func _ready() -> void:
 	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
 
 
 func increment_experience(xp: float):
-	current_experience += xp
-	print(current_experience)
+	current_experience = min(current_experience + xp, target_experience)
+	experience_updated.emit(current_experience, target_experience, current_level)
+	if current_experience == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experience = 0
+		play_experience_bar_animation.emit()
+		experience_updated.emit(current_experience, target_experience, current_level)
 	
 	
 func on_experience_vial_collected(xp: float):
