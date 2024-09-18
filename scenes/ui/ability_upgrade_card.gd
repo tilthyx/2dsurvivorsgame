8f7c6688dd1208ad43c88cb28612fb38e780adb6
tilthyx <tilthyx@gmail.com>
2024-09-18 03:03:46 -0300

    Feature Upgrade: Make the upgrade select in UI

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label: Label = %NameLabel
 @onready var label_description: Label = %DescriptionLabel
 
@@ -7,3 +9,8 @@ extends PanelContainer
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	label_description.text = upgrade.description
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		selected.emit()
