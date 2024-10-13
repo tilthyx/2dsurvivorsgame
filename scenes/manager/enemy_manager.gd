6b77013f1c134f69bf631ad70ba7fde44b13bc4d
tilthyx <tilthyx@gmail.com>
2024-10-13 16:30:31 -0300

    Feature Axe: Adding axe ability

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -13,20 +13,35 @@ func _ready() -> void:
 	base_spawn_time = timer.wait_time
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return Vector2.ZERO
+
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+		var query_paramaters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position,1)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramaters)
+		
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
+		
 
 func _on_timer_timeout() -> void:
 	timer.start()
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if player == null:
-		return
-		
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
 	
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities = get_tree().get_first_node_in_group("entities_layer")
 	entities.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 	
 func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (0.1 / 12) * arena_difficulty
